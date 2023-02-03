521e2dfb6a6f164a64d8a1a846ae7fe09714cec3
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-03 12:29:34 -0300

    Classe LogRecords

---
+++ exercResolvidoSet/Entities/LogRecord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercResolvidoSet.Entities
+{
+    internal class LogRecord
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public LogRecord(string userName, DateTime instant)
+        {
+            UserName = userName;
+            Instant = instant;
+        }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode() + Instant.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is LogRecord)) return false;
+            LogRecord other = obj as LogRecord;
+            return UserName.Equals(other.UserName) && Instant.Equals(other.Instant);
+        }
+        
+
+    }
+}
